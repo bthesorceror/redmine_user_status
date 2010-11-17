747b6f6fe01f5f01ac766b6413ca51739897785f
Brandon F <thepooreskimojoe@gmail.com>
2010-11-16 22:55:04 -0600

    moving to version 0.0.2

--- init.rb
+++ init.rb
@@ -13,7 +13,7 @@ Redmine::Plugin.register :redmine_user_status do
   name 'Redmine User Status plugin'
   author 'Brandon Farmer'
   description 'Allows you to find out what co workers are doing'
-  version '0.0.1'
+  version '0.0.2'
   permission :feed_view, {:user_status => [:show_feed]}, :public => true
   menu :top_menu, :user_status, {:controller => 'user_status', :action => 'index'}, :caption => 'Statuses', :if => Proc.new { User.current.logged? }
 
