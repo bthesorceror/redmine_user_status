8eca91e83ef5b5560dadf12394a94a594c662b6b
Brandon F <thepooreskimojoe@gmail.com>
2010-11-18 22:38:10 -0600

    fix for configuration page

--- init.rb
+++ init.rb
@@ -15,8 +15,8 @@ Redmine::Plugin.register :redmine_user_status do
   description 'Allows you to find out what co workers are doing'
   version '0.0.2'
  
-  settings :default => {'user_status_expiry' => ''}, :partial => 'settings/settings'
-  settings :default => {'user_status_limit' => 100}, :partial => 'settings/settings'
+  settings :default => {'user_status_expiry' => ''}, :partial => 'settings/user_status_settings'
+  settings :default => {'user_status_limit' => 100}, :partial => 'settings/user_status_settings'
 
   permission :feed_view, {:user_status => [:show_feed]}, :public => true
   menu :top_menu, :user_status, {:controller => 'user_status', :action => 'index'}, :caption => 'Statuses', :if => Proc.new { User.current.logged? }
